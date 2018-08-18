0c67ec29587aec3d71e9a20fdd99842cecdaadb3
Daniel Duan <daniel@duan.ca>
2018-08-18 10:01:16 -0700

    Update package description to Swift 4.1

--- Package.swift
+++ Package.swift
@@ -1,9 +1,14 @@
+// swift-tools-version:4.1
 import PackageDescription
 
 let package = Package(
     name: "Termbox",
+    products: [
+        .library(name: "Termbox", targets: ["Termbox"]),
+        .library(name: "ctermbox", targets: ["ctermbox"]),
+    ],
     targets: [
-        Target(name: "Termbox", dependencies: ["ctermbox"]),
-        Target(name: "ctermbox"),
+        .target(name: "Termbox", dependencies: ["ctermbox"]),
+        .target(name: "ctermbox"),
     ]
 )
